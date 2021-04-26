1c782b7a8ff4cccaaa2b017e356740a9183d4309
Fernando Gabriel <56029704+ferganiki@users.noreply.github.com>
2021-04-26 11:42:59 -0300

    Add files via upload

---
+++ Atividade10/janela.js
@@ -0,0 +1,15 @@
+function abrirJanela(){
+    document.getElementById("janela").src="janelaAberta.png";
+}
+
+function fecharJanela(){
+    document.getElementById("janela").src="janelaFechada.png";
+}
+
+function quebrarJanela(){
+    document.getElementById("janela").src="janelaQuebrada.png";
+}
+
+function fecharParaQuebrar(){
+    document.getElementById("janela").src="janelaFechada.png";
+}
